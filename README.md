f3a6abecd697b3cb1ccf17c12093607a3cebd653
Max Fowler <maxhfowler@gmail.com>
2017-07-25 19:57:40 -0400

    Example

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # osf-example
+
+Example of how to use the osf python library
+
+# setup
+
+```
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+
+# run
+
+Configure variables in run.py, and then run:
+```
+python run.py
+```
---
+++ run.py
@@ -0,0 +1,26 @@
+import datetime
+
+from osf.scrapers.facebook import FbScraper
+
+if __name__ == '__main__':
+
+    # initialize scraper
+    scraper = FbScraper(
+        fb_username='your-fb-email',    # the username you use to login to facebook with
+        fb_password='your-fb-password'  # the password you use to login to facebook with
+    )
+
+    # fetch friends
+    friends = scraper.get_friends_of_user(
+        user='your-fb-username'     # the sting that appears in the URL when you navigate to your fb profile
+    )
+    print friends
+
+    # fetch posts
+    posts = scraper.get_posts(params={
+        # list of usernames to pull posts from (as returned by get_friends above),
+        'users': ['user1', 'user2'],
+        # will only return posts after this date (in this case, posts in the last 7 days)
+        'after_date': datetime.datetime.now() - datetime.timedelta(days=7)
+    })
+    print posts
